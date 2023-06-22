9d4a08e198b08dc2af3a53cc42de7484e6a31f55
velsanjay <sanjaymech2313@mail.com>
2023-06-22 12:46:59 +0530

    commit

---
+++ database.js
@@ -0,0 +1,30 @@
+let mysql = require('mysql')
+const GetDetail = require('express').Router()
+
+GetDetail.get('/',async(req,res)=>{
+
+    let connection = mysql.createPool({
+        host:'localhost',
+        database:'employee_database',
+        user:'root',
+        password:'root123'
+    })
+    
+    connection.query(`select * from employee_info`,(err, result, fields)=>{
+        if(err){ 
+            res.status(401).send({
+                successs:false,
+                err
+            })
+        }else{
+            res.status(200).send({
+                successs:true,
+                data:result
+            })
+        }
+    
+        })
+})
+
+    
+module.exports = GetDetail
